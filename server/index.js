6fc207523edcc2c2e3b713386a69d580f1cfc885
Shehroz <shehrozalidanish@gmail.com>
2026-06-09 18:22:07 +0500

    Use hosted server port

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const path = require('path');
 const { v4: uuidv4 } = require('uuid');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const DATA_FILE = path.join(__dirname, 'data.json');
 
 app.use(cors());
